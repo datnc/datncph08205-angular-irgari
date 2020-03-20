d217840d94e706b3d026822be175b38a5a494e36
datnc <hipboylc@gmail.com>
2020-03-20 16:04:53 +0700

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ProductManagerComponent } from './product-manager/product-manager.component';
 
 
 const routes: Routes = [
-  {path: 'add-product', component : AddProductComponent
-  },
+  {path: 'add-product', component : AddProductComponent},
   {path: 'about',component:AboutComponent},
   {path: 'home',component:HomeComponent},
+  {path: 'manager',component:ProductManagerComponent}
 ];
 
 @NgModule({
