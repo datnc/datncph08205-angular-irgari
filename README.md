6ff42e0925c37eec616b76bb2c41100d01f25102
datnc <hipboylc@gmail.com>
2020-03-20 08:51:15 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# datncph08205-angular-irgari
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/datncph08205-angular-irgari)
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { SlideComponent } from './slide/slide.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductManagerComponent } from './product-manager/product-manager.component';
+import { ProductService } from './service/product.service';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, SlideComponent, ProductComponent, ProductListComponent, CategoryComponent, ProductManagerComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ProductService]
+})
+export class AppModule { }
